a381c82964e207f664fc4b850c280c98d8b21b08
Fatih Gumus <fatihgumus72@gmail.com>
2024-06-25 19:55:36 +0300

    Initial Commit

---
+++ SwiftNavigationStack/SwiftNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNavigationStackApp.swift
+//  SwiftNavigationStack
+//
+//  Created by Fatih Gümüş on 25.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
